d38105cdb07dcbf99644e552b6105f276b7c71ad
AymanElshehawy <elshehawy.m92@gmail.com>
2024-02-08 17:49:32 +0200

    Initiate project with backpack

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,6 +67,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'admins' => AdminsMiddleware::class,
+//        'admins' => AdminsMiddleware::class,
     ];
 }
