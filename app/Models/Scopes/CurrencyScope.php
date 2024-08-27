df27955ec603d9e49cab8a73ec0503a72be3f119
Mostafa Arafat <163301569+mostafaarafat199@users.noreply.github.com>
2024-08-27 06:55:03 +0300

    update:: able to change the country and get advertisements dependent on it

--- app/Models/Scopes/CurrencyScope.php
+++ app/Models/Scopes/CurrencyScope.php
@@ -13,9 +13,16 @@ class CurrencyScope implements Scope
      */
     public function apply(Builder $builder, Model $model): void
     {
+
         // get currency from header or default to SAR
         $country_key = request()->header('country-key', 'SA');
         $currency = $country_key == 'SA' ? 'SAR' : ($country_key == 'EG' ? 'EGP' : 'AED');
+
+
+        //Egypt == (country_id = 1) & KSA == (country_id = 2) & UAE == (country_id = 3)
+        if (session()->has('country_id')) {
+            $currency = session()->get('country_id') == '1' ? 'EGP' : (session()->get('country_id') == '2' ? 'SAR' : 'AED');
+        }
 
         // filter by currency
         $builder->where('currency', $currency);
