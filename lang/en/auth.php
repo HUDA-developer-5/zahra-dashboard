bca318fa2db85f329b7e5c77b0d0ba3b65d1fded
mostafaarafat199 <mostafaarafat199@gmail.com>
2024-09-10 13:50:33 +0300

    fix(issue):: translations

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -16,5 +16,6 @@ return [
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'Please review the terms and conditions and agree to create an account.' => 'Please review the terms and conditions and agree to create an account.',
 
 ];
