6db3147a5949d2fc6286787d37ebc98d1cbee6c7
Mostafa Arafat <163301569+mostafaarafat199@users.noreply.github.com>
2024-10-06 22:50:51 +0300

    push

--- app/Models/Scopes/CurrencyScope.php
+++ app/Models/Scopes/CurrencyScope.php
@@ -13,7 +13,6 @@ class CurrencyScope implements Scope
      */
     public function apply(Builder $builder, Model $model): void
     {
-
         // get currency from header or default to SAR
         $country_key = request()->header('country-key', 'SA');
         $currency = $country_key == 'SA' ? 'SAR' : ($country_key == 'EG' ? 'EGP' : 'AED');
